c8285d92915a62a6858b51b92b3c39087d958047
vahanoi <vahanoi@gmail.com>
2018-11-18 17:31:12 +0000

    command line options few tests

--- palba/palba.py
+++ palba/palba.py
@@ -4,8 +4,9 @@ Created on 18 Nov 2018
 Palba startup file
 Python 3
 
-@author: vahan
+@author: vahanoi
 '''
+import sys
 
 
 def palba():
@@ -18,11 +19,16 @@ def main ():
         
     
     '''
-#TODO: check input options from command line
-    while True:
-        #TODO: test
-        #FIXME: testfixme
-        None
-
+#TODO: check input options from command line sys.argv[]
+    while True:      
+        if len(sys.argv)==1:
+            print ('Entering default mode - analyse /var/log folder')
+            break
+        else:
+            print('Entering command line mode - Check command line options')
+            print (len(sys.argv))
+            for option in sys.argv:
+                print (str(option))
+            break
 if __name__ == '__main__':
     main()
