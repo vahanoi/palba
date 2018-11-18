23950e3e349b948644b736820752dbb39e02e003
vahanoi <vahanoi@gmail.com>
2018-11-18 08:10:44 +0000

    Import from old files

--- palba/palba.py
+++ palba/palba.py
@@ -1,8 +1,28 @@
 '''
 Created on 18 Nov 2018
 
+Palba startup file
+Python 3
+
 @author: vahan
 '''
 
+
+def palba():
+    None
+    
+def main ():
+    ''' Start a parser and run a main program loop
+        pylogsparser - parsing library with XML normalizers -  by Wallix
+        why to rewrite it?? Just use existing Just find out how to use
+        
+    
+    '''
+#TODO: check input options from command line
+    while True:
+        #TODO: test
+        #FIXME: testfixme
+        None
+
 if __name__ == '__main__':
-    pass
+    main()
