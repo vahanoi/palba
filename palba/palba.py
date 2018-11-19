a6b5e65816087d38dc63c9f6bf5adad26b8d7033
vahanoi <vahanoi@gmail.com>
2018-11-19 21:16:50 +0000

    more in options

--- palba/palba.py
+++ palba/palba.py
@@ -7,6 +7,7 @@ Python 3
 @author: vahanoi
 '''
 import sys
+from _ast import If
 
 
 def palba():
@@ -16,7 +17,9 @@ def main ():
     ''' Start a parser and run a main program loop
         pylogsparser - parsing library with XML normalizers -  by Wallix
         why to rewrite it?? Just use existing Just find out how to use
-        
+        Options:
+            -o then c for csv (-oc), h for html (-oh)
+            ...
     
     '''
 #TODO: check input options from command line sys.argv[]
@@ -28,6 +31,11 @@ def main ():
             print('Entering command line mode - Check command line options')
             print (len(sys.argv))
             for option in sys.argv:
+                #TODO: Check options provided or create table with them
+                if str(option)=='-oh':
+                    None
+                elif str(option)=='-oc':
+                    None    
                 print (str(option))
             break
 if __name__ == '__main__':
