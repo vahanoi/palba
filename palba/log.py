4924c5bc24b62ef67f9977612708f742c902446e
vahanoi <vahanoi@gmail.com>
2018-11-24 16:25:21 +0000

    add log.py

---
+++ palba/log.py
@@ -0,0 +1,18 @@
+'''
+Created on 22 Nov 2018
+
+@author: vahanoi
+'''
+
+class logfile(object):
+    '''
+    operations on logfile - open, close, analyse, check type, convert? 
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+    def logline(self):
+        None
