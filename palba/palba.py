1f7da0f85c4ed23360b7e8a5de20bc8bd15cffd5
vahanoi <vahanoi@gmail.com>
2020-09-15 19:24:03 +0100

    Arg parser changes

--- palba/palba.py
+++ palba/palba.py
@@ -8,6 +8,14 @@ Python 3
 @author: vahanoi
 '''
 import sys
+import log
+import getopt
+import datetime
+import re
+
+
+DefaultFolder='/var/log/nginx'
+
 
 def loadl():
     #TODO: check if file exist and open in read only mode
@@ -16,7 +24,7 @@ def loadl():
 def palba():
     None
     
-def main ():
+def main (argv):
     ''' Start a parser and run a main program loop
         pylogsparser - parsing library with XML normalizers -  by Wallix
         why to rewrite it?? Just use existing Just find out how to use
@@ -26,10 +34,22 @@ def main ():
             ...
     
     '''
+        # Get startup options using getopt
+    try:
+        opts, args = getopt.getopt(argv,"hi:o:d:D:",["ifile=","ofile=","date=","directory="])
+    except getopt.GetoptError as err: # FIXME: not throwing error correctly - 
+        print(err)
+        print('Error use format palba.py -i <inputfile>|<ipnutfolder> -o <outputfile> -d YYYY/MM/dd')
+        sys.exit(2)
+
 #TODO: check input options from command line sys.argv[]
+     
+    for o,a in opts:
+        if o == -D or o == --Directory:
+            None 
     while True:      
         if len(sys.argv)==1:
-            print ('Entering default mode - analyse /var/log folder')
+            print ('Entering default mode - analyse %s folder' % DefaultFolder)
             break
         else:
             print('Entering command line mode - Check command line options')
@@ -43,4 +63,4 @@ def main ():
                 print (str(option))
             break
 if __name__ == '__main__':
-    main()
+    main(sys.argv[1:])
